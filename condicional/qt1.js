233110bf9c5a467d0b69efe72b30a5b710639cbc
jdanielf <danielljd@hotmail.com>
2025-08-20 21:11:45 -0300

    atualiuzação

--- condicional/qt1.js
+++ condicional/qt1.js
@@ -1,4 +1,16 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync()
 
-let 
+let velcarr= Number(prompt("Digite a velocidade do carro: "))
+let velvia= Number(prompt("Digite a velocidade permitida na via: "))
+
+let velvia10= velvia+(velvia*0.10)
+
+if(velcarr>velvia && velcarr<=velvia10){
+console.log("Atenção: acima do permitido, multa leve!")
+
+}if(velcarr>velvia10){
+    console.log("Atenção: acima do permitido, multa grave!")
+}else{
+    console.log("Velocidade dentro do limite.")
+}
---
+++ condicional/qt2.js
@@ -0,0 +1,5 @@
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
+let cons, não , sim
+
