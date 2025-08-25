de4fc47f3e95cc64c00a386496496befaaee3327
Rafaella Sabino Silva <Rafaella@danielj.ferreira>
2025-08-25 15:36:53 -0300

    2 e 3

--- estrutura escolha/qt2.js
+++ estrutura escolha/qt2.js
@@ -1,5 +1,58 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync()
 
-let oper= prompt("Digite a operação que deseja realizar - básica ou avançada: ")
+let tipo= prompt("Digite o tipo que deseja realizar - básica ou avançada: ").toLowerCase()
 
+if (tipo ==="basica"){
+    let oper= prompt("Agora,escolha entre as operações que deseja o resultado - Soma, subitração, multiplicação ou divisão: ").toLowerCase()
+    let num1=Number(prompt("Digite o primeiro número: "))
+    let num2=Number(prompt("Digite o segundo número: "))
+
+
+    switch(oper){
+        case "soma": 
+        console.log("A soma dos números é " +(num1+num2).toFixed(2))
+        break
+        case "subtracao": 
+        console.log("A soma dos números é " +(num1-num2).toFixed(2))
+        break
+        case "multiplicacao": 
+        console.log("A soma dos números é " +(num1*num2).toFixed(2))
+        break
+        case "divisao":
+            if(num2<=0){
+               console.log("O denominador, deve ser maior que 0") 
+            } else{
+                console.log("A divisão dos números é " +(num1/num2).toFixed(2))
+            }
+        console.log("A soma dos números é " +(num1+num2).toFixed(2))
+        break
+        default:
+            console.log("Operação ou números inválidos'")
+        break
+  }
+}else if(tipo==="avancada"){
+     let oper= prompt("Agora,escolha entre as operações que deseja o resultado - Raiz quadrada ou potência: ").toLowerCase()
+      switch(oper){
+        case "raiz quadrada":
+            let num1=Number(prompt("Digite o número para saber a riaz quadrada: "))
+            if(num1<=0){
+                console.log("Digite um número maior que zero!")
+            }
+            else{
+                console.log("A raiz quadrada do número "+num1+ " é "+ Math.sqrt(num1).toFixed(2))
+            }break
+        case "potencia":
+            let base=Number(prompt("Digite o valor da base: "))
+            let exp=Number(prompt("Digite o valor do expoente: "))
+
+            console.log("A base "+base+ " elevado ao expoente "+exp+ " é "+ Math.pow(base,exp).toFixed(2))
+         break
+         default:
+            console.log("Operação ou números inválidos'")  
+
+      }
+
+  }else{
+    console.log("Escolha entre básica ou avançada")
+  }
---
+++ estrutura escolha/qt3.js
@@ -0,0 +1,33 @@
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
+console.log("Responda as perguntas apenas com sim ou não - ")
+let re1 = prompt("Telefonou para a vítima? ").toLowerCase()
+let re2 = prompt("Esteve no local do crime? ").toLowerCase()
+let re3 = prompt("Mora perto da vítima? ").toLowerCase()
+let re4 = prompt("Devia para a vítima? ").toLowerCase()
+let re5 = prompt("Já trabalhou com a vítima? ").toLowerCase()
+
+if((re1!="sim" && re1!="nao") || (re2!="sim" && re2!="nao") || (re3!="sim" && re3!="nao")||
+    (re4!="sim" && re4!="nao")||(re5!="sim" && re5!="nao")){
+        console.log("Digite apenas sim ou não para as perguntas!")
+    }else{
+switch(true){
+    case re1==="sim" && re2==="sim" && re3==="sim" && re4==="sim" && re5==="sim" :
+        console.log("Assassino")
+        break
+    case (re1==="sim" && re2==="sim" && re3==="sim")||(re1==="sim" && re2==="sim" && re4==="sim")||
+         (re1==="sim" && re2==="sim" && re5==="sim")||(re2==="sim" && re3==="sim" && re4==="sim")||
+         (re2==="sim" && re3==="sim" && re5==="sim")||(re3==="sim" && re4==="sim" && re5==="sim"):
+        console.log("Cúmplice")
+        break
+    case (re1==="sim" && re2==="sim")||(re1==="sim" && re3==="sim")||(re1==="sim" && re4==="sim")||
+         (re1==="sim" && re5==="sim")||(re2==="sim" && re3==="sim")||(re2==="sim" && re4==="sim")||
+         (re2==="sim" && re5==="sim")||(re3==="sim" && re4==="sim")||(re3==="sim" && re5==="sim")||
+         (re4==="sim" && re5==="sim"):
+         console.log("Suspeito(a)")
+         break
+    default:
+        console.log("Inocente")
+}
+}
