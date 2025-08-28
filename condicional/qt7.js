8ee4599b380a84a6445323e17d4c097b37419e07
Rafaella Sabino Silva <Rafaella@danielj.ferreira>
2025-08-28 12:18:42 -0300

    7

--- condicional/qt7.js
+++ condicional/qt7.js
@@ -1,18 +1,12 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync()
 
-<<<<<<< HEAD
 let ano= Number(prompt("Digite o ano que deseja saber se é bisexto, com 4 digitos: "))
-
-if(ano/4==0){
-    
-
-=======
+if(ano/4==0){  
 let ano=Number(prompt("Digite o ano, com 4 digitos, para saber se é bissexto: "))
-
-if((ano%4===0 && ano%100!==0) || (ano%400===0)){
+}if((ano%4===0 && ano%100!==0) || (ano%400===0)){
     console.log("O ano "+ano+" é bissexto.")
 }else{
     console.log("O ano "+ano+" não é bissexto.")
->>>>>>> 90d878f95e2982e9ba253ad638147a82518f9446
+
 }
