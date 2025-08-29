144c66d7e8c2e9fdf358b2d3dcfa8a6798743fbd
jdanielf <danielljd@hotmail.com>
2025-08-28 21:13:34 -0300

    5

--- repetição/qt5.js
+++ repetição/qt5.js
@@ -1,3 +1,21 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync()
 
+let sal = 510, aum = 1.5, bon, sla2011
+
+console.log("2010    ", aum, "     ",sal )
+for(let i = 2010 ; i<=2025 ; i++ ){
+    if(i==2011){
+        console.log(i,"   ",((sal*aum)/100),"     ",sal+(sal*aum/100) )  
+        sla2011 = sal+(sal*aum/100)
+    }else if(i>2011 && i<=2025){
+       aum+=1.5
+       sla2011+=sla2011*(aum/100)
+        bon =(sla2011*aum)/100
+        
+       
+       console.log(i,"   ",aum,"     ",sla2011.toFixed(2) )
+        
+    }
+  
+}
