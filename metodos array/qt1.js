985f7aebd5dd613aab537c10797c2d6f8c05786d
jdanielf <danielljd@hotmail.com>
2025-09-05 21:08:46 -0300

    início

---
+++ metodos array/qt1.js
@@ -0,0 +1,18 @@
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
+let tex, jorn=[], ofen=["merda", "droga", "bomba", "lula"], i
+
+tex = prompt(`Digite o texto do artigo para publicação: `)
+jorn=tex.split(" ")
+
+for(i=0 ; i<= jorn.length ; i++){
+
+if(ofen[i]===jorn[i]){
+    let tdofe=[]
+}
+}
+
+console.log(tex)
+console.log(jorn)
+console.log(tdofe)
