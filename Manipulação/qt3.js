a1f4ab98a3e840a91d4ccbf5b737ef0ad1138afa
Daniel Ferreira <danielljd@hotmail.com>
2025-09-17 17:08:44 -0300

    qt3

---
+++ Manipulação/qt3.js
@@ -0,0 +1,23 @@
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
+let nomes = []
+
+while (true) {
+  let digi = prompt(`Digite os nomes dos funcionários e 0 para encerrar: `)
+
+  if (digi === "0") {
+    break
+  }
+  digi = digi.trim()
+    nomes.push(digi)
+  
+}
+
+if (nomes.length > 0) {
+  let sorteio = Math.floor(Math.random() * nomes.length)
+  console.log("\n--- RESULTADO DO SORTEIO ---")
+  console.log("Funcionário vencedor: " + nomes[sorteio])
+} else {
+  console.log("Nenhum nome válido informado!")
+}
